fa6fd015f27a6eb3454fa94d975296b6469aa371
jmackert <jonahbmackert@gmail.com>
2020-02-04 21:41:12 -0500

    made only grass around TCs

--- GenerateTownSpawn.cs
+++ GenerateTownSpawn.cs
@@ -10,35 +10,35 @@ public class GenerateTownSpawn
         string grassTile = "G";
         string townCenter = "TC";
         
-        int randX1 = Random.Range(0, rows);
-        int randY1 = Random.Range(0, columns / 4);
+        int randX1 = Random.Range(1, rows - 1);
+        int randY1 = Random.Range(1, columns / 4);
 
-        int randX2 = Random.Range(0, rows);
+        int randX2 = Random.Range(1, rows - 1);
         int randY2 = Random.Range(columns / 2, columns);
 
         while(buildingGrid [randX1,randY1] == null)
         {
-                if (tileGrid[randX1,randY1] == grassTile && resourceGrid[randX1,randY1] == null) 
+                if (tileGrid[randX1,randY1] == grassTile && tileGrid[randX1+1,randY1] == grassTile && tileGrid[randX1-1,randY1] == grassTile && tileGrid[randX1,randY1+1] == grassTile && tileGrid[randX1,randY1-1] == grassTile && resourceGrid[randX1,randY1] == null && resourceGrid[randX1+1,randY1] == null && resourceGrid[randX1-1,randY1] == null && resourceGrid[randX1,randY1+1] == null && resourceGrid[randX1,randY1-1] == null) 
                 {
                     buildingGrid[randX1,randY1] = townCenter;
                 }
                 else
                 {
-                    randX1 = Random.Range(0, rows);
-                    randY1 = Random.Range(0, columns / 4);
+                    randX1 = Random.Range(1, rows - 1);
+                    randY1 = Random.Range(1, columns / 4);
                 }
         }
 
         while(buildingGrid [randX2,randY2] == null)
         {
-                if (tileGrid[randX2,randY2] == grassTile && resourceGrid[randX2,randY2] == null) 
+                if (tileGrid[randX2,randY2] == grassTile && tileGrid[randX2+1,randY2] == grassTile && tileGrid[randX2-1,randY2] == grassTile && tileGrid[randX2,randY2+1] == grassTile && tileGrid[randX2,randY2-1] == grassTile && resourceGrid[randX2,randY2] == null && resourceGrid[randX2+1,randY2] == null && resourceGrid[randX2-1,randY2] == null && resourceGrid[randX2,randY2+1] == null && resourceGrid[randX2,randY2-1] == null) 
                 {
                     buildingGrid[randX2,randY2] = townCenter;
                 }
                 else
                 {
-                    randX2 = Random.Range(0, rows);
-                    randY2 = Random.Range(columns / 2, columns);
+                    randX2 = Random.Range(1, rows -1);
+                    randY2 = Random.Range(columns / 2, columns - 1);
                 }
         }
     }
